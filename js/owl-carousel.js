62a17c205e96f04d82928310b26539fc225e1c25
Your Name <you@example.com>
2021-02-07 03:14:34 +0400

    special_offers_section_added

---
+++ js/owl-carousel.js
@@ -0,0 +1,17 @@
+$(".owl-carousel.discounts").owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: false,
+  dot: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
